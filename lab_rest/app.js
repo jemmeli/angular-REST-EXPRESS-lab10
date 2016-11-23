a790b7b5da54bb779ee7960b7af90fe312beedd4
Dja <jemmeli84@gmail.com>
2016-11-23 09:58:40 +0100

    first commit

--- lab_rest/app.js
+++ lab_rest/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var quotes = require('./routes/quotes');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/quotes', quotes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

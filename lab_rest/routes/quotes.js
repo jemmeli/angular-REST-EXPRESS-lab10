a567787d0db19f7377db467097c9dc9258acafd8
Dja <jemmeli84@gmail.com>
2016-11-23 09:23:26 +0100

    first commit

--- lab_rest/routes/quotes.js
+++ lab_rest/routes/quotes.js
@@ -1,31 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var quotes = [
-    { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says'I'm possible'!"},
-    { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-    { author : 'Unknown', text : "Even the greatest was once a beginner. Don’t be afraid to take that first step."},
-    { author : 'Neale Donald Walsch', text : "You are afraid to die, and you’re afraid to live. What a way to exist."}
-];
+
+//use the service
+var service = require('../services/quoteservice');
+
 
 router.get('/', function(req, res) {
-    res.json(quotes);//get all quotes
+    res.json(service.findAll());
 });
 
-router.get('/random', function(req, res) {
 
-    var id = Math.floor(Math.random() * quotes.length);
-    var q = quotes[id];
+router.get('/random', function(req, res) {
+    //use service functions
+    var id = Math.floor(Math.random() * service.findAll().length);
+    //var q = quotes[id];
+    var q = service.findById(id);
     res.json(q);
 });
 
+
 router.get('/:id', function(req, res) {
-    if(quotes.length <= req.params.id || req.params.id < 0) {
+    if(service.findAll().length <= req.params.id || req.params.id < 0) {
         res.statusCode = 404;
         return res.send('Error 404: No quote found');
     }
-    var q = quotes[req.params.id];
+    // var q = quotes[req.params.id];
+    var q = service.findById(req.params.id);
     res.json(q);
 });
+
 
 router.post('/', function(req, res) {
     if(!req.body.hasOwnProperty('author') || !req.body.hasOwnProperty('text')) {
@@ -36,17 +39,19 @@ router.post('/', function(req, res) {
         author : req.body.author,
         text : req.body.text
     };
-
-    quotes.push(newQuote);
+    //quotes.push(newQuote);
+    service.create(newQuote);
     res.json(true);
 });
 
+
 router.delete('/:id', function(req, res) {
-    if(quotes.length <= req.params.id) {
+    if(service.findAll().length <= req.params.id) {
         res.statusCode = 404;
         return res.send('Error 404: No quote found');
     }
-    quotes.splice(req.params.id, 1);
+    // quotes.splice(req.params.id, 1);
+    service.remove(req.params.id);
     res.json(true);
 });
 
